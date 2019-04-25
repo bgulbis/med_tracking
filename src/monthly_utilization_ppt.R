e74171483c2c9ab59ef987ca457ab7505f89a089
Brian Gulbis <brian.gulbis@gmail.com>
2019-04-25 13:25:16 -0500

    iv apap graphs; save to ppt

--- src/monthly_utilization_ppt.R
+++ src/monthly_utilization_ppt.R
@@ -12,6 +12,14 @@ library(sweep)
 tz_locale <- locale(tz = "US/Central")
 floor_unit <- "month"
 
+campus <- c(
+    "HC Childrens",
+    "HH HERMANN",
+    "HH Radiology",
+    "HH Rehab",
+    "HH Trans Care"
+)
+
 get_data <- function(path, pattern, col_types = NULL) {
     f <- list.files(path, pattern, full.names = TRUE)
     
@@ -28,7 +36,7 @@ get_data <- function(path, pattern, col_types = NULL) {
 }
 
 make_forecast <- function(ts, h = 12) {
-    forecast::auto.arima(ts) %>%
+    forecast::auto.arima(ts, lambda = 0) %>%
         forecast::forecast(h) %>%
         sweep::sw_sweep(timetk_idx = TRUE) %>%
         dplyr::mutate_at(
@@ -39,7 +47,69 @@ make_forecast <- function(ts, h = 12) {
         dplyr::mutate_at("key", stringr::str_to_title)
 }
 
+plot_utilization <- function(df) {
+    df %>%
+        ggplot(aes(x = med_date, y = n)) +
+        geom_line(size = 2) +
+        scale_x_datetime(NULL, date_breaks = "6 months", date_labels = "%b %Y") +
+        ylab("Number of Doses") +
+        # ggtitle(
+        #     paste0(
+        #         "Sugammadex usage predicted to increase by ", 
+        #         chg$change, 
+        #         "% over the next 12-months"
+        #     )
+        # ) +
+        theme_bg() +
+        theme(
+            axis.text.x = element_text(vjust = 0.1),
+            legend.position = "bottom"
+        )
+}
+
+plot_forecast <- function(df) {
+    df %>%
+        ggplot(aes(x = index, y = n)) +
+        geom_ribbon(aes(ymin = lo.95, ymax = hi.95), fill = "grey85", alpha = 0.5) +
+        geom_ribbon(aes(ymin = lo.80, ymax = hi.80), fill = "grey75", alpha = 0.5) +
+        geom_line(aes(color = key), size = 2) +
+        scale_color_manual(NULL, values = c("black", "blue")) +
+        scale_x_datetime(NULL, date_breaks = "6 months", date_labels = "%b %Y") +
+        ylab("Number of Doses") +
+        # ggtitle(
+        #     paste0(
+        #         "Sugammadex usage predicted to increase by ", 
+        #         chg$change, 
+        #         "% over the next 12-months"
+        #     )
+        # ) +
+        theme_bg() +
+        theme(
+            axis.text.x = element_text(vjust = 0.1),
+            legend.position = "bottom"
+        )
+}
+
 # acetaminophen ----------------------------------------
+
+dir_data <- "../acetaminophen_iv/data/tidy/mbo"
+
+data_apap_events <- get_data(dir_data, "apap_events")
+data_apap_orders <- get_data(dir_data, "apap_orders")
+
+df_apap <- data_apap_events %>%
+    filter(
+        facility_event %in% campus,
+        clinical_event_datetime >= mdy("4/1/2017")
+    ) %>%
+    mutate(med_date = floor_date(clinical_event_datetime, unit = floor_unit)) %>%
+    count(med_date) 
+    
+ts_apap <- tk_ts(df_apap)
+
+fcast_apap <- make_forecast(ts_apap)
+g_apap <- plot_utilization(df_apap)
+g_apap_fcast <- plot_forecast(fcast_apap)
 
 # albumin ----------------------------------------------
 
@@ -72,30 +142,68 @@ dir_data <- "data/tidy/pegfilgrastim"
 data_pegfilgrastim_events <- get_data(dir_data, "pegfilgrastim_events")
 data_pegfilgrastim_orders <- get_data(dir_data, "pegfilgrastim_orders")
 
-ts_pegf <- data_pegfilgrastim_events %>%
+df_pegf <- data_pegfilgrastim_events %>%
     mutate(med_date = floor_date(clinical_event_datetime, unit = floor_unit)) %>%
-    count(med_date) %>%
-    tk_ts()
+    count(med_date) 
+
+ts_pegf <- tk_ts(df_pegf)
 
 fcast_pegf <- make_forecast(ts_pegf)
+g_pegf <- plot_utilization(df_pegf)
+g_pegf_fcast <- plot_forecast(fcast_pegf)
 
-g_fcast_pegf <- fcast_pegf %>%
-    ggplot(aes(x = index, y = n)) +
-    geom_ribbon(aes(ymin = lo.95, ymax = hi.95), fill = "grey85", alpha = 0.5) +
-    geom_ribbon(aes(ymin = lo.80, ymax = hi.80), fill = "grey75", alpha = 0.5) +
-    geom_line(aes(color = key), size = 2) +
-    scale_color_manual(NULL, values = c("black", "blue")) +
-    scale_x_datetime(NULL, date_breaks = "6 months", date_labels = "%b %Y") +
-    ylab("Number of Doses") +
-    # ggtitle(
-    #     paste0(
-    #         "Sugammadex usage predicted to increase by ", 
-    #         chg$change, 
-    #         "% over the next 12-months"
-    #     )
-    # ) +
-    theme_bg() +
-    theme(
-        axis.text.x = element_text(vjust = 0.1),
-        legend.position = "bottom"
+# powerpoint -------------------------------------------
+
+slide_layout <- "Title and Content"
+slide_master <- "Office Theme"
+title_size <- fp_text(font.size = 32)
+
+# layout_properties(read_pptx(), layout = "Title Slide", master = slide_master)
+
+read_pptx() %>%
+    add_slide(layout = "Title Slide", master = slide_master) %>%
+    ph_with_text(type = "ctrTitle", str = "Utilization of Target Medications") %>%
+    ph_with_text(
+        type = "subTitle", 
+        str = paste0(
+            "Brian Gulbis, PharmD, BCPS\n", 
+            format(today(), "%B %e, %Y")
+        )
+    ) %>%
+    add_slide(layout = slide_layout, master = slide_master) %>%
+    ph_with("IV Acetaminophen", location = ph_location_type("title")) %>%
+    ph_with_vg(ggobj = g_apap, type = "body") %>%
+    add_slide(layout = slide_layout, master = slide_master) %>%
+    ph_with("Pegfilgrastim", location = ph_location_type("title")) %>%
+    ph_with_vg(ggobj = g_pegf, type = "body") %>%
+    print(
+        target = paste0(
+            "figs/target_med_utilization_",
+            format(today(), "%Y-%m"),
+            ".pptx"
+        )
+    )
+
+read_pptx() %>%
+    add_slide(layout = "Title Slide", master = slide_master) %>%
+    ph_with_text(type = "ctrTitle", str = "Forecast for Target Medications") %>%
+    ph_with_text(
+        type = "subTitle", 
+        str = paste0(
+            "Brian Gulbis, PharmD, BCPS\n", 
+            format(today(), "%B %e, %Y")
+        )
+    ) %>%
+    add_slide(layout = slide_layout, master = slide_master) %>%
+    ph_with("IV Acetaminophen", location = ph_location_type("title")) %>%
+    ph_with_vg(ggobj = g_apap_fcast, type = "body") %>%
+    add_slide(layout = slide_layout, master = slide_master) %>%
+    ph_with("Pegfilgrastim", location = ph_location_type("title")) %>%
+    ph_with_vg(ggobj = g_pegf_fcast, type = "body") %>%
+    print(
+        target = paste0(
+            "figs/target_med_forecast_",
+            format(today(), "%Y-%m"),
+            ".pptx"
+        )
     )
