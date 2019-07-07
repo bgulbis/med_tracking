66b75f4d3c8da65e4d346e6642a041dfd0d49eca
Brian Gulbis <brian.gulbis@gmail.com>
2019-07-07 03:17:11 +0000

    functions for generating pptx slides

--- src/pptx_slides.py
+++ src/pptx_slides.py
@@ -1,70 +1,115 @@
-# import numpy as np 
 import pandas as pd
-from pptx import Presentation
+from pptx import Presentation 
 from pptx.chart.data import CategoryChartData
 from pptx.dml.color import RGBColor
-from pptx.enum.chart import (XL_CHART_TYPE, XL_DATA_LABEL_POSITION,
-                             XL_LEGEND_POSITION, XL_MARKER_STYLE)
-from pptx.enum.dml import MSO_LINE_DASH_STYLE
+from pptx.enum.chart import XL_CHART_TYPE, XL_LEGEND_POSITION, XL_DATA_LABEL_POSITION, XL_MARKER_STYLE, XL_TICK_MARK
+from pptx.enum.dml import MSO_LINE_DASH_STYLE, MSO_THEME_COLOR
 from pptx.util import Inches, Pt
 
-# import datetime as datetime 
+def add_forecast_slide(p, df, med):
+    df = df.replace({pd.np.nan: None})
+    blank_slide_layout = p.slide_layouts[6]
+    slide = p.slides.add_slide(blank_slide_layout)
 
+    chart_data = CategoryChartData()
+    chart_data.categories = df.index
+    chart_data.add_series("Actual", df["Actual"])
+    chart_data.add_series("Forecast", df["Forecast"])
 
-prs = Presentation()
-# title slide
-title_slide_layout = prs.slide_layouts[0]
-slide = prs.slides.add_slide(title_slide_layout)
-title = slide.shapes.title
-subtitle = slide.placeholders[1]
+    x, y, cx, cy = Inches(1), Inches(1), Inches(8), Inches(6)
+    chart = slide.shapes.add_chart(XL_CHART_TYPE.LINE, x, y, cx, cy, chart_data).chart
+    chart = format_graph(chart, med)
+    return p
 
-title.text = "Test Forecast Slides"
-subtitle.text = "Updated: Some Date"
+def format_graph(chart, med, font_nm = None, ax_bright = 0.35):
+    category_axis = format_axis_title(chart.category_axis, "Month", font_nm, ax_bright)
+    category_axis = format_axis_tick_labels(category_axis, font_nm, ax_bright, "mmm yy")
+    category_axis = format_axis(category_axis, ax_bright)
+#     category_axis.maximum_scale = len(df_combined) + 1
+#     df_combined.asfreq("MS").index[-1] + pd.DateOffset(months=1)
 
-# forecast slide
-blank_slide_layout = prs.slide_layouts[6]
-slide = prs.slides.add_slide(blank_slide_layout)
+    value_axis = format_axis_title(chart.value_axis, "Number of doses", font_nm, ax_bright)
+    value_axis = format_axis_tick_labels(value_axis, font_nm, ax_bright, "#,##0")
+    value_axis = format_axis(value_axis, ax_bright)
 
-chart_data = CategoryChartData()
-#chart_data.categories = ['Q1 Sales', 'Q2 Sales', 'Q3 Sales']
+    if med == "ivig":
+        med_title = med.upper()
+    elif med == "acetaminophen":
+        med_title = med.title() + " IV "
+    else:
+        med_title = med.title()
+    
+    chart.has_title = True
+    chart.chart_title.text_frame.text = med_title + " forecast"
+    chart.chart_title.text_frame.paragraphs[0].font.name = font_nm
+    chart.chart_title.text_frame.paragraphs[0].font.size = Pt(24)
+    chart.chart_title.text_frame.paragraphs[0].font.bold = False
+    chart.chart_title.text_frame.paragraphs[0].font.color.theme_color = MSO_THEME_COLOR.TEXT_1
+    chart.chart_title.text_frame.paragraphs[0].font.color.brightness = 0.25
 
-date1 = '2017-07-01'
-date2 = '2017-12-01'
-chart_data.categories = pd.date_range(date1, date2, freq="MS").tolist()
+    chart.has_legend = False
+    # chart.legend.include_in_layout = False
+    # chart.legend.position = XL_LEGEND_POSITION.TOP
 
-chart_data.add_series('West',    (32.2, 28.4, 34.7))
-chart_data.add_series('East',    (24.3, 30.6, 20.2))
-chart_data.add_series('Midwest', (None, None, None, 20.4, 18.3, 26.2))
+    # format Actual line
+    col_actual = MSO_THEME_COLOR.ACCENT_1
+    chart.series[0].format.line.width = Pt(3.5)
+    chart.series[0].format.line.color.theme_color = col_actual
 
-x, y, cx, cy = Inches(1), Inches(1), Inches(8), Inches(6)
-chart = slide.shapes.add_chart(XL_CHART_TYPE.LINE, x, y, cx, cy, chart_data).chart
+    i = len(chart.series[0].points) - 13
+    chart = format_marker(chart, 0, i, font_nm, MSO_THEME_COLOR.BACKGROUND_1, col_actual)
 
-category_axis = chart.category_axis
-category_axis.has_title = True
-category_axis.axis_title.text_frame.text = "Month"
+    # format Forecast line
+    brght_forecast = 0.4
 
-value_axis = chart.value_axis
-value_axis.has_major_gridlines = False
-value_axis.has_minor_gridlines = False
+    chart.series[1].format.line.dash_style = MSO_LINE_DASH_STYLE.DASH
+    chart.series[1].format.line.width = Pt(2.5)
+    chart.series[1].format.line.color.theme_color = col_actual
+    chart.series[1].format.line.color.brightness = brght_forecast
 
-chart.has_legend = True
-chart.legend.include_in_layout = False
-chart.legend.position = XL_LEGEND_POSITION.TOP
+    j = len(chart.series[1].points) - 1
+    chart = format_marker(chart, 1, j, font_nm, col_actual, col_actual, brght_forecast)
+    
+    return chart
 
-chart.series[0].smooth = True
+def format_axis(axis, ax_bright):
+    axis.format.line.color.theme_color = MSO_THEME_COLOR.TEXT_1
+    axis.format.line.color.brightness = ax_bright
+    axis.has_major_gridlines = False
+    axis.has_minor_gridlines = False
+    return axis
 
-chart.series[1].format.line.dash_style = MSO_LINE_DASH_STYLE.DASH
-chart.series[1].format.line.width = Pt(1.5)
+def format_axis_title(axis, title_text, font_nm, ax_bright, has_title = True):
+    axis.has_title = has_title
+    axis.axis_title.text_frame.text = title_text
+    axis.axis_title.text_frame.paragraphs[0].font.name = font_nm
+    axis.axis_title.text_frame.paragraphs[0].font.size = Pt(18)
+    axis.axis_title.text_frame.paragraphs[0].font.bold = False
+    axis.axis_title.text_frame.paragraphs[0].font.color.theme_color = MSO_THEME_COLOR.TEXT_1
+    axis.axis_title.text_frame.paragraphs[0].font.color.brightness = ax_bright
+    return axis
 
-i = len(chart.series[-1].points) - 1
-chart.series[-1].points[i]
+def format_axis_tick_labels(axis, font_nm, ax_bright, num_fmt):
+    axis.tick_labels.font.name = font_nm
+    axis.tick_labels.font.size = Pt(16)
+    axis.tick_labels.font_bold = False
+    axis.tick_labels.font.color.theme_color = MSO_THEME_COLOR.TEXT_1
+    axis.tick_labels.font.color.brightness = ax_bright
+    axis.tick_labels.number_format = num_fmt
+    return axis
 
-chart.series[-1].points[i].marker.style = XL_MARKER_STYLE.CIRCLE
-chart.series[-1].points[i].marker.format.fill.solid()
-chart.series[-1].points[i].marker.format.fill.fore_color.rgb = RGBColor.from_string("FFFFFF")
-chart.series[-1].points[i].marker.format.line.width = Pt(1.5)
-chart.series[-1].points[i].data_label.has_text_frame = True
-chart.series[-1].points[i].data_label.position = XL_DATA_LABEL_POSITION.BELOW
-chart.series[-1].points[i].data_label.text_frame.text = str(round(chart.series[-1].values[-1], None))
-
-prs.save('doc/forecast.pptx')
+def format_marker(chart, i, j, font_nm, col_mark, col_line, brght=0):
+    chart.series[i].data_labels.number_format = "#,##0"
+#     chart.series[i].data_labels.show_value = True
+    chart.series[i].data_labels.font.name = font_nm
+    chart.series[i].data_labels.font.size = Pt(18)
+    chart.series[i].data_labels.font.bold = False
+    chart.series[i].data_labels.position = XL_DATA_LABEL_POSITION.BELOW
+    chart.series[i].points[j].marker.style = XL_MARKER_STYLE.CIRCLE
+    chart.series[i].points[j].marker.format.fill.solid()
+    chart.series[i].points[j].marker.format.fill.fore_color.theme_color = col_mark
+    chart.series[i].points[j].marker.format.fill.fore_color.brightness = brght
+    chart.series[i].points[j].marker.format.line.width = Pt(2.5)
+    chart.series[i].points[j].marker.format.line.color.theme_color = col_line
+    chart.series[i].points[j].marker.format.line.color.brightness = brght
+    return chart
