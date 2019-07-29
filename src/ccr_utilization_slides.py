ca91f42559feedc6e7a228ddf3f5d011abb5ad4d
Brian Gulbis <brian.gulbis@gmail.com>
2019-07-29 09:11:04 -0500

    remove chart formatting; use chart template

--- src/ccr_utilization_slides.py
+++ src/ccr_utilization_slides.py
@@ -29,28 +29,13 @@ def add_utilization_slide(p, df, med):
 
     chart_data = CategoryChartData()
     chart_data.categories = df.index
-    # chart_data.categories.number_format = "mmmm"
 
     # for i in range(0, len(df.columns)):
     for i in range(len(df.columns) - 1, -1, -1):
-        chart_data.add_series(df.columns[i], df.iloc[:, i], "#,##0")
+        chart_data.add_series(df.columns[i], df.iloc[:, i])
 
     x, y, cx, cy = Inches(1), Inches(1), Inches(8), Inches(6)
     chart = slide.shapes.add_chart(XL_CHART_TYPE.LINE, x, y, cx, cy, chart_data).chart
-    chart = format_graph(chart, med)
-    return p
-
-def format_graph(chart, med, font_nm=None, ax_bright=0.5):
-    category_axis = format_axis_title(chart.category_axis, "Month", font_nm, ax_bright)
-    category_axis = format_axis_tick_labels(category_axis, font_nm, ax_bright, "mmm")
-    category_axis = format_axis(category_axis, ax_bright)
-
-    ax_max = date(2020, 6, 30) - date(1899, 12, 30)
-    category_axis.maximum_scale = ax_max.days  # 6/30/2020
-
-    value_axis = format_axis_title(chart.value_axis, "Doses per month", font_nm, ax_bright)
-    value_axis = format_axis_tick_labels(value_axis, font_nm, ax_bright, "#,##0")
-    value_axis = format_axis(value_axis, ax_bright)
 
     if med == "ivig":
         med_title = med.upper()
@@ -63,68 +48,12 @@ def format_graph(chart, med, font_nm=None, ax_bright=0.5):
     else:
         med_title = med.title()
 
-    chart.has_title = True
     chart.chart_title.text_frame.text = med_title + " utilization"
-    chart.chart_title.text_frame.paragraphs[0].font.name = font_nm
-    chart.chart_title.text_frame.paragraphs[0].font.size = Pt(24)
-    chart.chart_title.text_frame.paragraphs[0].font.bold = False
-    chart.chart_title.text_frame.paragraphs[0].font.color.theme_color = MSO_THEME_COLOR.TEXT_1
-    chart.chart_title.text_frame.paragraphs[0].font.color.brightness = 0.25
+    # chart.category_axis.axis_title.text_frame.text = "Month"
+    chart.value_axis.has_title = True
+    chart.value_axis.axis_title.text_frame.text = "Doses per month"
 
-    chart.has_legend = False
-
-    col_list = [MSO_THEME_COLOR.TEXT_1,
-                MSO_THEME_COLOR.ACCENT_1,
-                MSO_THEME_COLOR.ACCENT_2,
-                MSO_THEME_COLOR.ACCENT_3,
-                MSO_THEME_COLOR.ACCENT_4]
-    brght_prev = 0.6
-
-    for i in range(0, len(chart.series)):
-        if i == 0:
-            w = Pt(3.5)
-        else:
-            w = Pt(2.25)
-
-        chart.series[i].format.line.width = w
-        chart.series[i].format.line.color.theme_color = col_list[i]
-        chart.series[i].data_labels.font.name = font_nm
-        chart.series[i].data_labels.font.size = Pt(16)
-        chart.series[i].data_labels.font.bold = False
-        chart.series[i].data_labels.font.color.theme_color = col_list[i]
-        chart.series[i].data_labels.position = XL_DATA_LABEL_POSITION.RIGHT
-
-        if i >= 1:
-            chart.series[i].format.line.color.brightness = brght_prev
-            chart.series[i].data_labels.font.color.brightness = brght_prev
-
-    return chart
-
-def format_axis(axis, ax_bright):
-    axis.format.line.color.theme_color = MSO_THEME_COLOR.TEXT_1
-    axis.format.line.color.brightness = ax_bright
-    axis.has_major_gridlines = False
-    axis.has_minor_gridlines = False
-    return axis
-
-def format_axis_title(axis, title_text, font_nm, ax_bright, has_title=True):
-    axis.has_title = has_title
-    axis.axis_title.text_frame.text = title_text
-    axis.axis_title.text_frame.paragraphs[0].font.name = font_nm
-    axis.axis_title.text_frame.paragraphs[0].font.size = Pt(18)
-    axis.axis_title.text_frame.paragraphs[0].font.bold = False
-    axis.axis_title.text_frame.paragraphs[0].font.color.theme_color = MSO_THEME_COLOR.TEXT_1
-    axis.axis_title.text_frame.paragraphs[0].font.color.brightness = ax_bright
-    return axis
-
-def format_axis_tick_labels(axis, font_nm, ax_bright, num_fmt):
-    axis.tick_labels.font.name = font_nm
-    axis.tick_labels.font.size = Pt(16)
-    axis.tick_labels.font_bold = False
-    axis.tick_labels.font.color.theme_color = MSO_THEME_COLOR.TEXT_1
-    axis.tick_labels.font.color.brightness = ax_bright
-    axis.tick_labels.number_format = num_fmt
-    return axis
+    return p
 
 def make_slides(meds):
     prs = Presentation("../doc/template.pptx")
@@ -162,5 +91,21 @@ meds = ["acetaminophen-iv",
         "ertapenem",
         "meropenem-vaborbactam"]
 
-prs = make_slides(meds)
-prs.save('../report/utilization/python_utilization_slides.pptx')
+# prs = make_slides(meds)
+
+prs = Presentation("../doc/template.pptx")
+# title slide
+title_slide_layout = prs.slide_layouts[0]
+slide = prs.slides.add_slide(title_slide_layout)
+title = slide.shapes.title
+subtitle = slide.placeholders[1]
+title.text = "Utilization of Target Medications"
+data_end = read_data(meds[0]).index[-1].strftime('%B %Y')
+subtitle.text = "Data through: " + data_end + "\nBrian Gulbis, PharmD, BCPS"
+
+for i in meds:
+    df = read_data(i)
+    df = prep_df(df)
+    add_utilization_slide(prs, df, i)
+
+prs.save('../report/utilization/python_utilization_slides_no-formatting.pptx')
