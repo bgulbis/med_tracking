00eada7b22f44720c14a7d43d63d0146712cfddf
Brian Gulbis <brian.gulbis@gmail.com>
2022-01-14 11:26:30 -0600

    add utilization chart formatting

--- src/pptx_utilization_target_meds.R
+++ src/pptx_utilization_target_meds.R
@@ -28,7 +28,7 @@ my_theme <- mschart_theme(
 ts_doses <- df_meds |>
     mutate(across(dose_month, as.Date)) |>
     group_by(medication, product, dose_month) |>
-    summarize(across(c(patients, doses, quantity), sum, na.rm = TRUE)) |>
+    summarize(across(c(patients, doses, quantity), sum, na.rm = TRUE), .groups = "drop_last") |>
     mutate(month = yearmonth(dose_month)) |>
     as_tsibble(key = c(medication, product), index = month) |>
     fill_gaps(doses = 0L) |>
@@ -48,30 +48,81 @@ df_data <- ts_doses |>
     filter(fiscal_year >= curr_fy - 3) |>
     mutate(
         across(fiscal_qtr, ~format(., "FY%y")),
-        across(fiscal_qtr, ~if_else(!is.na(product), paste(., product, sep = "-"), .))
+        across(fiscal_qtr, ~if_else(!is.na(product), paste(., product, sep = "-"), .)),
+        group_color = case_when(
+            fiscal_year == curr_fy ~ "#000000",
+            fiscal_year == curr_fy - 1 ~ "#A6CEE3",
+            fiscal_year == curr_fy - 2 ~ "#FDBF6F",
+            fiscal_year == curr_fy - 3 ~ "#B2DF8A"
+        ),
+        line_style = if_else(product == "Procrit", "dashed", "solid", "solid"),
+        line_width = if_else(fiscal_year == curr_fy, 3.5, 2.25)
     ) |>
-    select(medication, dose_month, doses, fiscal_year, fy = fiscal_qtr, fiscal_date)
+    select(medication, dose_month, doses, fiscal_year, fy = fiscal_qtr, fiscal_date, group_color, line_style, line_width) |> 
+    add_row(medication = "Spacer", doses = 0, fy = "FY", fiscal_date = mdy("6/1/2019"), group_color = "#FFFFFF", line_style = "none", line_width = 0) |> 
+    add_row(medication = "Spacer", doses = 0, fy = "FY", fiscal_date = mdy("7/1/2020"), group_color = "#FFFFFF", line_style = "none", line_width = 0)
+
+# x <- df_data |>
+#     filter(medication == "Acetaminophen IV") 
+# 
+# y <- x |>
+#     distinct(fy, line_style) |>
+#     deframe()
+# 
+# l <- map(y, ~fp_text(color = .x, font.size = 14, font.family = "Calibri"))
+# 
 
 add_chart <- function(pptx, m, 
                       slide_layout = "Title and Chart", 
                       slide_master = "Office Theme", 
-                      title_loc <- ph_location_label("Title 1"),
-                      chart_loc <- ph_location_label("Content Placeholder 2")) {
+                      title_loc = ph_location_label("Title 1"),
+                      chart_loc = ph_location_label("Chart Placeholder 7")) {
     # ph_location(left = 0.5, top = 1, width = 9, height = 6)
     
-    lc <- df_data |>
-        filter(medication == m) |>
+    df <- filter(df_data, medication %in% c(m, "Spacer")) 
+
+    group_colors <- df |> 
+        distinct(fy, group_color) |> 
+        deframe()
+    
+    line_styles <- df |> 
+        distinct(fy, line_style) |> 
+        deframe()
+
+    line_widths <- df |> 
+        distinct(fy, line_width) |> 
+        deframe()
+
+    data_labels <- map(group_colors, ~fp_text(color = .x, font.size = 14, font.family = "Calibri"))
+    
+    lc <- df |>
         ms_linechart(x = "fiscal_date", y = "doses", group = "fy") |>
         chart_data_smooth(0) |>
-        chart_ax_x(num_fmt = "[$-en-US]mmm;@") |>
-        chart_ax_y(num_fmt = "#,##0") |>
+        chart_settings(style = "line") |>
+        chart_ax_x(num_fmt = "[$-en-US]mmm;@", major_tick_mark = "none") |> # cross_between = "midCat", major_tick_mark = "in"
+        chart_ax_y(num_fmt = "#,##0", major_tick_mark = "none") |>
         chart_labels(title = "Doses") |>
+        chart_data_fill(values = group_colors) |>
+        chart_data_stroke(values = group_colors) |>
+        chart_data_line_style(values = line_styles) |>
+        chart_data_line_width(values = line_widths) |>
+        chart_labels_text(values = data_labels) |>
         set_theme(my_theme)
+    
+    slide_title_format <- fp_text(color = "#404040", font.size = 24, bold = FALSE, font.family = "Calibri")
+    slide_title <- fpar(ftext(paste(m, "utilization"), slide_title_format))
+    
+    g <- ggplot() +
+        annotate("rect", xmin = 0, xmax = 1, ymin = 0, ymax = 1, fill = "white", color = "white") +
+        theme_void()
     
     pptx |>
         add_slide(layout = slide_layout, master = slide_master) |>
-        ph_with(value = paste(m, "utilization"), location = title_loc) |>
-        ph_with(value = lc, location = chart_loc)
+        ph_with(value = slide_title, location = title_loc) |>
+        ph_with(value = lc, location = chart_loc) |> 
+        ph_with(value = g, location = ph_location(left = 1, top = 6.7, width = 0.6, height = 0.3)) |> 
+        ph_with(value = g, location = ph_location(left = 8.8, top = 6.7, width = 0.5, height = 0.3))
+    
 }
 
 
@@ -187,6 +238,7 @@ p_sug_encntr_6m <- df_sug |>
 
 my_theme <- mschart_theme(
     grid_major_line = fp_border(width = 0),
+    double_fmt = "#,##0",
     date_fmt = "[$-en-US]mmm yyyy;@",
     # main_title = fp_text(color = "#404040", font.size = 24, bold = FALSE, font.family = "Calibri"),
     main_title = fp_text(color = "#595959", font.size = 16, bold = FALSE, font.family = "Calibri"),
@@ -196,8 +248,8 @@ my_theme <- mschart_theme(
     legend_text = fp_text(color = "#7F7F7F", font.size = 14, bold = FALSE, font.family = "Calibri")
 )
 
-# slide_layout <- "Blank"
-slide_layout <- "Title and Chart"
+slide_layout <- "Blank"
+# slide_layout <- "Title and Chart"
 slide_master <- "Office Theme"
 
 
@@ -205,6 +257,11 @@ add_bars <- function(pptx, value) {
     pptx |>
         ph_with(value = value, location = ph_location(left = 0.5, top = 1, width = 9, height = 6))
 }
+
+test_pptx <- read_pptx("doc/template.pptx")
+layout_properties(test_pptx, layout = "Title and Chart")
+# layout_properties(test_pptx, layout = "Title Slide")
+
 
 pptx <- read_pptx("doc/template.pptx") |>
     add_slide(layout = "Title Slide", master = slide_master) |>
